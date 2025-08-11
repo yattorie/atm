a3eeac8f171fba54f5a5e63c68242bce787bb501
yattorie <pyryryqw882@gmail.com>
2025-08-11 21:10:50 +0300

    refactor: move App to atm directory

---
+++ src/main/java/com/orlovandrei/atm/App.java
@@ -0,0 +1,10 @@
+package com.orlovandrei.atm;
+
+import com.orlovandrei.atm.api.ATMApp;
+
+public class App {
+    public static void main(String[] args) {
+        ATMApp atmApp = new ATMApp();
+        atmApp.startATM();
+    }
+}
